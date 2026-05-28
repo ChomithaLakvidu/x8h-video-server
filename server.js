dc640d250b4e3acdebbf6533a90a43a3d5a1fb7c
ChomithaLakvidu <chomithalakvidu0@gmail.com>
2026-05-28 11:58:43 +0530

    Initialize WebSocket server with Express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+app.get("/", (req, res) => { res.send("Server is running!"); });
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
